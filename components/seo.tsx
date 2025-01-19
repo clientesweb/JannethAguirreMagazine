dfc70c04274d3a3c8b9f6459669132ca559a9a10
clientesweb <alexisifran32@gmail.com>
2025-01-19 03:55:40 -0300

    Actualizar seo.tsx

--- components/seo.tsx
+++ components/seo.tsx
@@ -23,17 +23,15 @@ export default function SEO({ title, description, canonicalUrl, ogImage }: SEOPr
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={ogImage} />
       
-      {/* PWA meta tags */}
+      {/* Theme colors and PWA meta tags */}
+      <meta name="theme-color" content="#FF0000" />
+      <meta name="msapplication-TileColor" content="#FF0000" />
       <meta name="application-name" content="Janneth Aguirre Magazine" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="default" />
       <meta name="apple-mobile-web-app-title" content="JA Magazine" />
       <meta name="format-detection" content="telephone=no" />
       <meta name="mobile-web-app-capable" content="yes" />
-      <meta name="msapplication-config" content="/browserconfig.xml" />
-      <meta name="msapplication-TileColor" content="#FF0000" />
-      <meta name="msapplication-tap-highlight" content="no" />
-      <meta name="theme-color" content="#FF0000" />
 
       <link rel="apple-touch-icon" href="/icon-192x192.png" />
       <link rel="apple-touch-icon" sizes="152x152" href="/icon-152x152.png" />
@@ -45,12 +43,6 @@ export default function SEO({ title, description, canonicalUrl, ogImage }: SEOPr
       <link rel="manifest" href="/manifest.json" />
       <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#FF0000" />
       <link rel="shortcut icon" href="/favicon.ico" />
-      
-      {/* You may need to adjust these URLs based on your actual image locations */}
-      <meta name="twitter:image" content="https://yourdomain.com/og-image.jpg" />
-      <meta property="og:image" content="https://yourdomain.com/og-image.jpg" />
-      <meta property="og:image:width" content="1200" />
-      <meta property="og:image:height" content="630" />
     </Head>
   )
 }
