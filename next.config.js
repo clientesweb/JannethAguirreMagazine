95674a8e5e2ac0bd63214255a353869f2edd5350
clientesweb <alexisifran32@gmail.com>
2025-01-19 02:45:43 -0300

    Actualizar next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,3 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
-})
-
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -19,13 +10,7 @@ const nextConfig = {
     config.resolve.alias['@'] = path.resolve(__dirname)
     return config
   },
-  pwa: {
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development',
-  },
 }
 
-module.exports = withPWA(nextConfig)
+module.exports = nextConfig
 
