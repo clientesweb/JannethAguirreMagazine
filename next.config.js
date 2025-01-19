7e5a023f4e97c5d7b0882c002988cc52e131b68a
clientesweb <alexisifran32@gmail.com>
2025-01-19 01:42:08 -0300

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,25 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+})
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['images.unsplash.com'],
+    unoptimized: true,
+  },
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
+  },
+}
+
+module.exports = withPWA(nextConfig)
+
